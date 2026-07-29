127d629cef995be77ad4a486d9d95505906b678d
kinjalrathwark-cpu <kinjalrathwa.rk@gmail.com>
2026-07-29 14:58:35 +0530

    Add files via upload

---
+++ U1-P1.py
@@ -0,0 +1,40 @@
+NLP  = {'Domainname': "NLP",
+'Description': "Natural language processing ",
+'Realword': "text summariser"  ,
+'libraries': "nltk,spacy" }
+
+ComputerVision={
+'Domain name' : "NLP",
+'Description ': "Natural language processing ",
+'Real-Word' : "text summariser" ,
+'libraries' : "nltk,spacy" }
+
+SpeechProcessing={
+'Domain name' : "NLP",
+'Description ': "Natural language processing ",
+'Real-Word' : "text summariser" ,
+'libraries' : "nltk,spacy" }
+
+Robotics={
+'Domain name' : "NLP",
+'Description ': "Natural language processing ",
+'Real-Word' : "text summariser" ,
+'libraries' : "nltk,spacy" }
+
+
+print('Please seclt your option')
+print('1.NLP')
+print('2.Coputer vision')
+print('3.Speech Processing')
+print('4.Robotics')
+
+choic=int(input('Enter your number'))
+
+if choic==1:
+    print('Domain name : "NLP" ')
+elif choic==2:
+    print('Description : "Natural language processing" ')
+elif choic==3:
+    print('Real-Word : "text summariser" ')
+elif choic==4:
+    print('libraries : "nltk,spacy" ')
