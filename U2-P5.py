419a2f3383dda73c6197dd69ca286ca47be8ae53
kinjalrathwark-cpu <kinjalrathwa.rk@gmail.com>
2026-08-19 23:20:51 +0530

    Update U2-P5.py

--- U2-P5.py
+++ U2-P5.py
@@ -2,30 +2,49 @@ from google import genai
 from dotenv import load_dotenv
 import os
 import json
-import time 
+
 load_dotenv()
 
+FILE="history.json"
+
+if not os.path.exists(FILE):
+    with open(FILE, "w", encoding="utf-8") as file:
+        json.dump([], file, indent=4)
+
+with open(FILE, "r", encoding="utf-8") as file:
+    data=json.load(file)
 
 client = genai.Client(
     api_key=os.getenv("genai_api_key")
 )
 
+messages = []
+prompt = input("Enter system prompt")
+
+while True:
+        user_prompt = input("Enter your question:")
+        if user_prompt.lower() == "exit":
+             break
+        data.append({"type": "user_input","content": [{"type": "text" , "text": user_prompt}]})
+
 interaction = client.interactions.create (
-    model="gemini-3.7-flash",
-    input="Explain how AI works in a few words"
-    messages=[{'system ':'user','Assistant':}]
+     system_instruction = prompt,
+    model="gemini-3.7-flash-lite",
+    input="data"
 )
 
-system_prompt = input(
-    "Enter your System Prompt "
-    "(press Enter for default): "
-).strip()
+assistant_response = interaction.output_text
+data.append ({"type":"model_output","content":[{"type":"text","text" : assistant_response}]})
+print("Gemini:",assistant_response)
+total_messages = len(data)//2
+print("Total Messages:" , total_messages)
+print("Total tokens:" , dict(interaction["usage"]) ["total_tokens"])
 
-if not system_prompt:
-    system_prompt = (
-        "You are a helpful AI assistant. "
-        "Give clear and useful answers."
-    )
-print(interaction.output_text)
+with open(FILE, "w", encoding="uft-8") as file:
+     json.dump(data,file,indent=4)
+
+
+
+
 
 
