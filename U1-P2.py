6bd0ef7c1a308a7ab5b4b90bcfcfa37d3fb4ae77
kinjalrathwark-cpu <kinjalrathwa.rk@gmail.com>
2026-08-19 15:45:40 +0530

    Update U1-P2.py

--- U1-P2.py
+++ U1-P2.py
@@ -10,47 +10,40 @@ gamini = {'Develpore' : 'Google',
           'open Source' : 'No',
           'multimodel Support': 'Text, Images, Audio, Video, Code',
           'Typical use case ': 'Search, productivity, document understanding, coding, multimodal AI assistants'} 
-Llama3 = {'Develpore' : 'Meta',
+llama3 = {'Develpore' : 'Meta',
            'open Source ': 'Yes (open-weight with community license)',
            'multimodel Support' : 'Text (Llama 3), Text + Images (Llama 3.2 Vision)'
            'Typical use case :  Research, fine-tuning, local AI applications, chatbots, coding'} 
-Mistral = {'Develpore' : 'Mistral AI',
+mistral = {'Develpore' : 'Mistral AI',
           'open Source' : 'Partially',
           'multimodel Support' : 'Text, Images, Audio',
           'Typical use case' : 'Chatbots, enterprise AI, coding assistants, document analysis, on-premise AI deployments'}
 
+print('gpt4')
+print('claude3')
+print('gamini')
+print('llama3')
+print('mistral')
+
+choice_Models= input("Enter Topic Name :")
+choiceModels = choice_Models.upper()
 
 
-choiceModels=int(input("1.GPT-4 ,2.Clude 3, 3.Gemini, 4.Llama 3, 5.Mistral "
-                       "\nEnter Youre Choice:"))
-
-
-
-if  choiceModels == 1: 
-    print("\nGPT-4")
-    for key, value in gpt4.items():
-           print(key, ":" ,value)
-
-elif choiceModels == 2:
-        print("\nClude 3")
-        for key,value in claude3.items():
-               print(key, ":" ,value)
-
-elif choiceModels == 3:
-        print("Gamini")
-        for key,value in gamini.items():
-                print(key, ":" ,value)
-
-elif choiceModels == 4:
-        print("Llama 3")
-        for key,value in Llama3.items():
-                print(key, ":" ,value)
-
-elif choiceModels == 5:
-        print("Mistral")
-        for key,value in Mistral.items():
-                print(key, ":" ,value)
-
+if  choiceModels == 'gpt4' or  choiceModels == 'GPT 4': 
+    print(gpt4)
+    
+elif choice_Models == 'claude3' or choiceModels == 'Claude 3':
+        print(claude3)
+        
+elif choice_Models == 'gamini' or choiceModels == 'Gamini':
+        print(gamini)
+        
+elif choice_Models == 'llama3' or choiceModels == 'Llama 3':
+        print(llama3)
+        
+elif choice_Models == 'mistral' or choiceModels == 'Mistral':
+        print(mistral)
+        
 else:
        print("Invalid model choice!")
 
@@ -60,17 +53,7 @@ else:
 
 
 
-#create a python application to compare major Large language Models.
-# include the following model:
-#GPR-4
-#cluded 3
-#gemini
-#Llama 3
-#Mistral
 
 
-#Display
-#Develpore
-#open Source
-#multimodel Support
-#Typical use case 
+
+
